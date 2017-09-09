18f5d19ca3686ef10c87de05eb7e16090254638d
Andrey Dybov <dybov.andrew@gmail.com>
2017-09-10 00:40:08 +0500

    Create base, header, navigation and privacy-policy tempalates.
    Create base.html template for using in all apps
    Create exactly the same header as in initial web site (Customer's wish).
    Also add empty template for privacy policy.
    Add navbar and put base, header and navigation in a separate folder.
    Also add some styles which will be changed after.

--- real_estate_agency/real_estate_agency/settings.py
+++ real_estate_agency/real_estate_agency/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'real_estate',
 ]
 
@@ -115,5 +116,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
