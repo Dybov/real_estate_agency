9b71799bfa269a5cfd2bccb5171029455f512cc0
Andrey Dybov <dybov.andrew@gmail.com>
2017-09-30 00:41:42 +0500

    Modify admin interface for customer.
    Change styles, add new titles.
    Register models.

--- real_estate_agency/real_estate/admin.py
+++ real_estate_agency/real_estate/admin.py
@@ -1,3 +1,28 @@
-from django.contrib import admin
+#from django.contrib import admin
+from django.contrib.admin import AdminSite
+from django.contrib.admin import ModelAdmin
+from .models import ResidentalComplex, Apartment, Address
+
+from django.utils.translation import ugettext as _
+
+class MyAdminSite(AdminSite):
+    site_header = _('Администрирование DОМУС')
+    site_title  = _('DОМУС')
+
+admin = MyAdminSite(name='admin')
 
 # Register your models here.
+
+class ResidentalComplexAdmin(ModelAdmin):
+	list_display =  ('name', 'active','link_to_address')
+	list_editable = ('active',)
+
+class AddressAdmin(ModelAdmin):
+	list_display =  ('__str__','link_to_residentalcomplex',)
+
+class ApartmentAdmin(ModelAdmin):
+	list_display =  ('apartment_number','rooms', 'address', )
+
+admin.register(ResidentalComplex, ResidentalComplexAdmin)
+admin.register(Address, AddressAdmin)
+admin.register(Apartment, ApartmentAdmin)
--- real_estate_agency/real_estate/apps.py
+++ real_estate_agency/real_estate/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
-
+from django.utils.translation import ugettext_lazy as _
 
 class RealEstateConfig(AppConfig):
     name = 'real_estate'
+    verbose_name=_('Недвижимость')
