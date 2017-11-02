56619bd358d01f089fddfa9294755a86df506a88
Andrey Dybov <dybov.andrew@gmail.com>
2017-11-02 23:21:38 +0500

    Add templates from frontender.
    Add static filesand templates from frontender. Set up base template.

--- real_estate_agency/new_buildings/static/new_buildings/js/common.js
+++ real_estate_agency/new_buildings/static/new_buildings/js/common.js
@@ -1,5 +1,87 @@
 $(function() {
 
+$(".toggle-mnu").click(function() {
+  $(this).toggleClass("on");
+  $(".menu_container").slideToggle();
+  return false;
+});
+
 	// Custom JS
+$('.slider_item').magnificPopup({
+  type: 'image',
+    fixedContentPos: false,
+    fixedBgPos: true,
+    overflowY: 'auto',
+    closeBtnInside: true,
+    preloader: false,
+    midClick: true,
+    mainClass: 'my-mfp-zoom-in',
+    gallery:{
+    enabled:true,
+     }
 
 });
+
+    $('.callback').magnificPopup({
+    type: 'inline',
+    fixedContentPos: false,
+    fixedBgPos: true,
+    overflowY: 'auto',
+    closeBtnInside: true,
+    preloader: false,
+    midClick: true,
+    removalDelay: 300,
+    mainClass: 'my-mfp-zoom-in',
+    focus: '#popup_input'
+    });
+    $('.pop_up_plan').magnificPopup({
+    type: 'inline',
+    fixedContentPos: false,
+    fixedBgPos: true,
+    overflowY: 'auto',
+    closeBtnInside: true,
+    preloader: false,
+    midClick: true,
+    removalDelay: 300,
+    mainClass: 'my-mfp-zoom-in',
+    focus: '#popup_input'
+    });
+
+    $('.slider_soc_prog_item').magnificPopup({
+    type: 'inline',
+    fixedContentPos: false,
+    fixedBgPos: true,
+    overflowY: 'auto',
+    closeBtnInside: true,
+    preloader: false,
+    midClick: true,
+    removalDelay: 300,
+    mainClass: 'my-mfp-zoom-in',
+    focus: '#popup_input'
+    });
+
+$('.popup_video').magnificPopup({
+  type: 'iframe',
+    fixedContentPos: false,
+    fixedBgPos: true,
+    overflowY: 'auto',
+    closeBtnInside: true,
+    preloader: false,
+    midClick: true,
+    mainClass: 'my-mfp-zoom-in'
+
+});
+
+
+});
+
+(function() {
+  var toggle = document.querySelector("#flexy-nav__toggle");
+  var nav = document.querySelector("#flexy-nav__items");
+  toggle.addEventListener("click", function(e) {
+    e.preventDefault();
+    nav.classList.contains("flexy-nav__items--visible") ? nav.classList.remove("flexy-nav__items--visible") : nav.classList.add("flexy-nav__items--visible");
+  });
+})();
+
+
