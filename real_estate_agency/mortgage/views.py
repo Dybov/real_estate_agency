247a1ccffe5a052c6bf7e32513f809875c204206
Andrey Dybov <dybov.andrew@gmail.com>
2017-12-10 00:32:29 +0500

    Modify mortgage calculations.
    Modify relations in the view.
    Add mortgage percentage and amount of mortgage to output.

--- real_estate_agency/mortgage/views.py
+++ real_estate_agency/mortgage/views.py
@@ -10,40 +10,46 @@ class index(FormView):
     form_class = MortgageForm
     template_name = 'mortgage/calculator.html'
     success_url = '...'
+    initial = {'full_price': 1200000,
+               'initial_fee_percentage': 30,
+               'years': 15,
+               }
 
     def get(self, request, *args, **kwargs):
         form_class = self.get_form_class()
-        form = form_class(request.GET or None)
+        form = form_class(request.GET or self.get_initial())
         context = self.get_context_data(**kwargs)
         context['form'] = form
         if form.is_valid():
             price = form.cleaned_data['full_price']
-            initial_fee = form.cleaned_data['initial_fee_percentage']/100
+            initial_fee = form.cleaned_data['initial_fee_percentage']
             years = form.cleaned_data['years']
-            context['monthly_payment'] = self.calculateMortgageAnnuityPaymentForSberbank(
-                price=price*(1-initial_fee),
+
+            calc_results = self.calculateMortgageAnnuityPaymentForSberbank(
+                full_price=price,
                 years=years,
+                initial_fee_perc=initial_fee,
             )
+            context.update(calc_results)
         return self.render_to_response(context)
 
-    def calculateMortgageAnnuityPaymentForSberbank(self, price, years):
-        lte_7years_percentage = Decimal(7.4)
-        gt_7years_percentage = Decimal(9.4)
+    def calculateMortgageAnnuityPaymentForSberbank(self, full_price, years, initial_fee_perc):
+        output_params = {'full_price': full_price,
+                         'years': years,
+                         'initial_fee_perc': initial_fee_perc,
+                         }
         if years <= 7:
-            return self.calculateMortgageAnnuityPayment(
-                price,
-                years,
-                lte_7years_percentage
-            )
-        else:
-            return self.calculateMortgageAnnuityPayment(
-                price,
-                years,
-                gt_7years_percentage
-            )
+            output_params['mortgage_percentage'] = Decimal(7.4)
+        return self.calculateMortgageAnnuityPayment(**output_params)
 
-    def calculateMortgageAnnuityPayment(self, price=0, years=0, percentage=Decimal(9.4)):
+    def calculateMortgageAnnuityPayment(self, full_price=0, years=0, initial_fee_perc=20, mortgage_percentage=Decimal(9.4)):
         from math import pow as mpow
+        price = full_price*(1-initial_fee_perc/100)
         months = years*12
-        percentage /= 1200
-        return price * percentage / (1 - Decimal(mpow(1 + percentage, -months)))
+        monthly_mortgage_proportion = mortgage_percentage / 1200
+        monthly_payment = price * monthly_mortgage_proportion / \
+            (1 - Decimal(mpow(1 + monthly_mortgage_proportion, -months)))
+        return {'monthly_payment': monthly_payment,
+                'mortgage_percentage': mortgage_percentage,
+                'amount_of_mortgage': price,
+                }
