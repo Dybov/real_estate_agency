25212a1ee06f6252a71b29d167900df08d97a290
Andrey Dybov <dybov.andrew@gmail.com>
2017-12-13 01:05:27 +0500

    Change mortgage form and its handling.
    Form now uses inintial fee in rubles.
    Add validation that fee mustn't be grater than full price.
    Set labels for templates.
    Modify mortgage calculation for Sberbank.

--- real_estate_agency/mortgage/forms.py
+++ real_estate_agency/mortgage/forms.py
@@ -1,22 +1,35 @@
 from django import forms
 from django.utils.translation import ugettext as _
+from django.core.exceptions import ValidationError
 
 
 class MortgageForm(forms.Form):
     full_price = forms.DecimalField(
-        label=_('Цена недвижимости, руб'),
+        label=_('Стоимость жилья, руб'),
         min_value=300000,
         decimal_places=1,
         widget =forms.NumberInput(attrs={'step': 1}),
     )
-    initial_fee_percentage = forms.DecimalField(
-        label=_('Первоначальный взнос, %'),
+    initial_fee = forms.DecimalField(
+        label=_('Первоначальный взнос, руб'),
         min_value=0,
-        max_value=100,
         decimal_places=1,
+        widget =forms.NumberInput(attrs={'step': 1}),
     )
     years = forms.IntegerField(
         label=_('Срок кредитования, г'),
         min_value=5,
         max_value=30,
     )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        # initial_fee must be lower than full_price
+        if self.is_valid():
+            initial_fee = cleaned_data.get('initial_fee')
+            full_price = cleaned_data.get('full_price')
+            if initial_fee >= full_price:
+                raise ValidationError(
+                    {'initial_fee': [_('Убедитесь, что это значение меньше стоимости жилья'), ]}
+                )
+        return cleaned_data
--- real_estate_agency/mortgage/views.py
+++ real_estate_agency/mortgage/views.py
@@ -11,7 +11,7 @@ class index(FormView):
     template_name = 'mortgage/calculator.html'
     success_url = '...'
     initial = {'full_price': 1200000,
-               'initial_fee_percentage': 30,
+               'initial_fee': int(1200000*0.2),
                'years': 15,
                }
 
@@ -22,34 +22,36 @@ class index(FormView):
         context['form'] = form
         if form.is_valid():
             price = form.cleaned_data['full_price']
-            initial_fee = form.cleaned_data['initial_fee_percentage']
+            initial_fee = form.cleaned_data['initial_fee']
             years = form.cleaned_data['years']
 
             calc_results = self.calculateMortgageAnnuityPaymentForSberbank(
                 full_price=price,
                 years=years,
-                initial_fee_perc=initial_fee,
+                initial_fee=initial_fee,
             )
             context.update(calc_results)
         return self.render_to_response(context)
-
-    def calculateMortgageAnnuityPaymentForSberbank(self, full_price, years, initial_fee_perc):
+    
+    def calculateMortgageAnnuityPaymentForSberbank(self, full_price, years, initial_fee):
         output_params = {'full_price': full_price,
                          'years': years,
-                         'initial_fee_perc': initial_fee_perc,
+                         'initial_fee': initial_fee,
                          }
         if years <= 7:
             output_params['mortgage_percentage'] = Decimal(7.4)
+        elif years <= 12:
+            output_params['mortgage_percentage'] = Decimal(7.9)
         return self.calculateMortgageAnnuityPayment(**output_params)
 
-    def calculateMortgageAnnuityPayment(self, full_price=0, years=0, initial_fee_perc=20, mortgage_percentage=Decimal(9.4)):
+    def calculateMortgageAnnuityPayment(self, full_price=0, years=0, initial_fee=0, mortgage_percentage=Decimal(9.4)):
         from math import pow as mpow
-        price = full_price*(1-initial_fee_perc/100)
+        price = full_price - initial_fee
         months = years*12
         monthly_mortgage_proportion = mortgage_percentage / 1200
         monthly_payment = price * monthly_mortgage_proportion / \
             (1 - Decimal(mpow(1 + monthly_mortgage_proportion, -months)))
         return {'monthly_payment': monthly_payment,
                 'mortgage_percentage': mortgage_percentage,
-                'amount_of_mortgage': monthly_payment*12*years,
+                'amount_of_mortgage': price,
                 }
