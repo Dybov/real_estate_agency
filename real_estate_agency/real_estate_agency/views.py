170293123c89696bbdcfca0cbc72233ea03f90c8
Andrey Dybov <dybov.andrew@gmail.com>
2018-01-21 04:48:06 +0500

    Add form content to the index page.

--- real_estate_agency/real_estate_agency/views.py
+++ real_estate_agency/real_estate_agency/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,render_to_response
 from django.template import RequestContext
 
 from new_buildings.models import Builder, ResidentalComplex, NewApartment
+from new_buildings.forms import SearchForm
 from feedback.models import Feedback
 from company.views import about_company_in_digits_context_processor
 
@@ -13,6 +14,7 @@ def index(request):
     feedbacks = Feedback.objects.all()[:4]
     context = {
         'feedbacks': feedbacks,
+        'form': SearchForm,
     }
     context.update(about_company_in_digits_context_processor(request))
     return render(request,
