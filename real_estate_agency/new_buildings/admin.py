2ab5143fcdb5ccf99c3542f81679cee1f202f211
Andrey Dybov <dybov.andrew@gmail.com>
2018-02-18 00:55:03 +0500

    Remove ApartmentInlines from newly added RC. Also use PEP8 to new_buildings.admin

--- real_estate_agency/new_buildings/admin.py
+++ real_estate_agency/new_buildings/admin.py
@@ -37,14 +37,17 @@ class NewApartmentInline(StackedInlineWithImageWidgetInline):
     formfield_overrides = standart_formfield_overrides
     # if it will be registred then show link
     show_change_link = True
+
     def formfield_for_manytomany(self, db_field, request, **kwargs):
         if db_field.name == "buildings":
             try:
                 parent_obj_id = request.resolver_match.args[0]
-                kwargs["queryset"] = NewBuilding.objects.filter(residental_complex=parent_obj_id)
+                kwargs["queryset"] = NewBuilding.objects.filter(
+                    residental_complex=parent_obj_id)
             except IndexError:
                 pass
         return super(NewApartmentInline, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
 
 class BuildingInlineFormSet(BaseInlineFormSet):
     def clean(self):
@@ -65,17 +68,18 @@ class BuildingInlineFormSet(BaseInlineFormSet):
 
             if output in unique_together_values:
                 if 'name' in unique_names:
-                    form.add_error(None, _('Поля "Имя дома" и в данном жк должны быть уникальны'))
+                    form.add_error(
+                        None, _('Поля "Имя дома" и в данном жк должны быть уникальны'))
                 else:
-                    form.add_error(None, _('Совокупность полей "Улица", "Номер дома" и "Корпус" должна быть уникальна для всех домов'))
+                    form.add_error(None, _(
+                        'Совокупность полей "Улица", "Номер дома" и "Корпус" должна быть уникальна для всех домов'))
             else:
                 unique_together_values.append(output)
-        
-        
+
         for form in self.forms:
             if form.cleaned_data:
                 checkIfUnique(form, unique_together)
-                    
+
 
 class BuildingInline(admin.StackedInline):
     form = NewBuildingForm
@@ -95,7 +99,7 @@ class BuildingInline(admin.StackedInline):
               'date_of_construction',
               'building_permit',
               'project_declarations'
-    )
+              )
 
 
 class SomeInlineFormSet(BaseInlineFormSet):
@@ -111,15 +115,17 @@ class SomeInlineFormSet(BaseInlineFormSet):
 
         # For the multiupload images. If form won't be saved - it'll be 500 error
         # So it is better have FK field be completed when form is saved
-        form.data[form.prefix + '-' + att_name] = getattr(pk_value, 'pk', pk_value)
+        form.data[form.prefix + '-' +
+                  att_name] = getattr(pk_value, 'pk', pk_value)
         if att_name.endswith('_id'):
-            form.data[form.prefix + '-' + att_name[:-3]] = getattr(pk_value, 'pk', pk_value)
-        
+            form.data[form.prefix + '-' + att_name[:-3]
+                      ] = getattr(pk_value, 'pk', pk_value)
+
         obj = form.save(commit=False)
         setattr(obj, att_name, getattr(pk_value, 'pk', pk_value))
         if commit:
             obj.save()
-        
+
         # form.save_m2m() can be called via the formset later on if commit=False
         if commit and hasattr(form, 'save_m2m'):
             form.save_m2m()
@@ -127,6 +133,7 @@ class SomeInlineFormSet(BaseInlineFormSet):
 
     def save_existing(self, form, instance, commit=True):
         return form.save(commit=commit)
+
 
 class ResidentalComplexImageInline(admin.TabularInline):
     model = ResidentalComplexImage
@@ -172,3 +179,16 @@ class ResidentalComplexAdmin(admin.ModelAdmin):
             'fields': ('front_image', 'video_link', 'presentation'),
         }),
     )
+
+    def get_inline_instances(self, request, obj=None):
+        """Override to exclude NewApartmentInline
+        from newly added forms"""
+        from copy import deepcopy
+
+        original_inlines = deepcopy(self.inlines)
+        if not obj:
+            self.inlines.pop(self.inlines.index(NewApartmentInline))
+
+        answer = super().get_inline_instances(request, obj=None)
+        self.inlines = original_inlines
+        return answer
