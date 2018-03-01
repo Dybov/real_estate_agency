deecbafd2d5721af27e54a9c3ad085567cbeff70
Andrey Dybov <dybov.andrew@gmail.com>
2018-03-02 02:05:19 +0500

    Add tests to ResidentalComplex.
    They covers methods get_lower_price and get_date_of_construction in many varieties.
    Include mock library for mocking File objs in models.

--- real_estate_agency/new_buildings/tests.py
+++ real_estate_agency/new_buildings/tests.py
@@ -1,8 +1,28 @@
-from django.test import TestCase
+import datetime
 
-from address.models import NeighbourhoodModel
+from django.core.files import File
+from django.test import TestCase, tag
 
 from .models import *
+from .helpers import get_quarter_verbose
+
+from address.models import StreetModel, NeighbourhoodModel
+
+
+import mock
+
+
+NULL_PRICE = ''
+NULL_DATE_OF_CONSTRUCTION = ''
+
+
+file_mock = mock.MagicMock(spec=File, name='FileMock')
+file_mock.name = 'test1.jpg'
+
+
+def get_image_file():
+    return file_mock
+
 
 def create_neighbourhood():
     _objects = NeighbourhoodModel.objects.all()
@@ -10,11 +30,13 @@ def create_neighbourhood():
         return NeighbourhoodModel.objects.create(name='МЫС')
     return _objects[0]
 
+
 def create_builder():
     _objects = Builder.objects.all()
     if not _objects:
         return Builder.objects.create(name='Снегири')
     return _objects[0]
+
 
 def create_type_of_complex():
     _objects = TypeOfComplex.objects.all()
@@ -22,52 +44,220 @@ def create_type_of_complex():
         return TypeOfComplex.objects.create(name='Жилой комплекс')
     return _objects[0]
 
-def create_RC():
-    return ResidentalComplex.objects.create(type_of_complex=create_type_of_complex(),
-                                            name='super',
-                                            description='asd',
-                                            builder=create_builder(),
-                                            is_active=True,
-                                            neighbourhood=create_neighbourhood(),
+
+def create_RC(name='RC', description='smthng', is_active=True, **kwargs):
+    """ Creates Residental Complex.
+    Adds builder, neighbourhood and type_of_complex objs automatically
+    if its not provides"""
+    builder = kwargs.pop('builder', create_builder())
+    neighbourhood = kwargs.pop('neighbourhood', create_neighbourhood())
+    type_of_complex = kwargs.pop('type_of_complex', create_type_of_complex())
+
+    return ResidentalComplex.objects.create(name=name,
+                                            description=description,
+                                            is_active=is_active,
+                                            builder=builder,
+                                            neighbourhood=neighbourhood,
+                                            type_of_complex=type_of_complex,
+                                            **kwargs
                                             )
 
-def create_apartment(rc, use_rc_builing=True):
-    NewApartment.objects.create(
-        layout=get_image_file(),
-        total_area=38,
-        price=10000,
-        building=rc.new_builings,
-        )
 
-class ResidentalComplexMethodTests(TestCase):
-    def test_get_lowest_price(self):
+def create_apartment(rc, price, total_area=38, **kwargs):
+    return NewApartment.objects.create(
+        residental_complex=rc,
+        total_area=total_area,
+        price=price,
+        layout=get_image_file(),
+        **kwargs
+    )
+
+
+def create_building(rc, building_number=1, **kwargs):
+    date_of_start_of_construction = kwargs.pop(
+        'date_of_start_of_construction', datetime.datetime.now())
+    street = kwargs.pop('street', create_street())
+
+    return NewBuilding.objects.create(
+        residental_complex=rc,
+        date_of_start_of_construction=date_of_start_of_construction,
+        street=street,
+        building=building_number,
+        **kwargs
+    )
+
+
+def create_street():
+    _objects = StreetModel.objects.all()
+    if not _objects:
+        return StreetModel.objects.create(
+            name='Street'
+        )
+    return _objects[0]
+
+
+def buildings_factory(rc, buildings_amount, no_active_buildings_amount=0):
+    """ Create set of NewBuilding objs"""
+    buildings = []
+    for i in range(buildings_amount):
+        buildings.append(create_building(
+                rc,
+                name=str(i),
+                building_number=i,
+                date_of_construction=datetime.datetime.now(),
+            )
+        )
+    for i in range(buildings_amount, 
+                   buildings_amount+no_active_buildings_amount):
+        date = datetime.datetime.now()-datetime.timedelta(days=180)
+        buildings.append(create_building(
+                rc,
+                name=str(i),
+                building_number=i,
+                date_of_construction=date,
+                is_active=False,
+            )
+        )
+    return buildings
+
+
+class RCBaseTest(TestCase):
+    def setUp(self):
+        self.RC = create_RC()
+
+
+@tag('get_lowest_price')
+class ResidentalComplexLowestPricesMethodTests(RCBaseTest):
+    def test_get_lowest_price_without_apartment(self):
         """
         test_get_lowest_price() should minimal price of apartments which
         can be shown at site (is_active=True) and which are presented
         in appropriate buildings (have m2m link and is_active=True for building)
         """
-        obj = create_RC()
-        rc = obj.get_lowest_price()
-        self.assertIsNone(rc)
-        # create_apartment(obj)
-        # rc = obj.get_lowest_price()
-        # self.assertEqual(10000)
+
+        price = self.RC.get_lowest_price()
+        self.assertEqual(price, NULL_PRICE)
+
+    def test_get_lowest_price_with_apartment_with_building(self):
+        apartment_price = 111
+
+        building = create_building(self.RC)
+        apartment = create_apartment(self.RC, apartment_price)
+        apartment.buildings = [building]
+        price = self.RC.get_lowest_price()
+        self.assertEqual(price, apartment_price)
+
+    def test_get_lowest_price_with_apartment_without_building(self):
+
+        apartment = create_apartment(self.RC, 222)
+        price = self.RC.get_lowest_price()
+        self.assertEqual(price, NULL_PRICE)
+
+    def test_get_lowest_price_with_apartment_with_building_noactive(self):
+        apartment_price = 333
+
+        building = create_building(self.RC, is_active=False)
+        apartment = create_apartment(self.RC, apartment_price)
+        apartment.buildings = [building]
+        price = self.RC.get_lowest_price()
+        self.assertEqual(price, NULL_PRICE)
+
+    def test_get_lowest_price_with_apartment_noactive_with_building(self):
+        apartment_price = 444
+
+        building = create_building(self.RC)
+        apartment = create_apartment(self.RC, apartment_price, is_active=False)
+        apartment.buildings = [building]
+        price = self.RC.get_lowest_price()
+        self.assertEqual(price, NULL_PRICE)
+
+    @tag('slow')
+    def test_get_lowest_price_with_few_apartments_in_one_builing(self):
+        lowest_price = 555
+        prices = [lowest_price, lowest_price+5, lowest_price+10]
+
+        building = create_building(self.RC)
+        for apartment_price in prices:
+            apartment = create_apartment(self.RC, apartment_price)
+            apartment.buildings = [building]
+        price = self.RC.get_lowest_price()
+        self.assertEqual(price, lowest_price)
+
+    @tag('slow')
+    def test_get_lowest_price_with_mixed_apartments_and_buildings(self):
+        [
+            building1,
+            building2,
+            building3,
+            building_no_active
+        ] = buildings_factory(self.RC, 3, 1)
+        apartment1 = create_apartment(self.RC, 666)
+        apartment1.buildings = [building1, building3, building_no_active]
+
+        apartment2 = create_apartment(self.RC, 667)
+        apartment2.buildings = [building2, building_no_active]
+
+        apartment3 = create_apartment(self.RC, 668)
+        apartment3.buildings = [building1,
+                                building2,
+                                building3,
+                                building_no_active]
+
+        apartment_no_active = create_apartment(self.RC, 660, is_active=False)
+        apartment_no_active.buildings = [building1,
+                                         building2,
+                                         building3,
+                                         building_no_active]
+
+        apartment_with_no_active_building = create_apartment(self.RC, 650)
+        apartment_with_no_active_building.buildings = [building_no_active]
+
+        price = self.RC.get_lowest_price()
+        self.assertEqual(price, 666)
 
 
-    def test_get_highest_price(self):
-        pass
+@tag('get_date_of_construction')
+class ResidentalComplexNearestDatesMethodTests(RCBaseTest):
+    def test_get_date_of_construction_without_buildings(self):
+        date_of_construction = self.RC.get_date_of_construction()
+        self.assertEqual(date_of_construction, NULL_DATE_OF_CONSTRUCTION)
 
+    def test_get_date_of_construction_with_building(self):
+        date_of_construction_of_building = datetime.datetime.now()
+        building = create_building(
+            self.RC,
+            date_of_construction=date_of_construction_of_building
+        )
 
+        date_of_construction = self.RC.get_date_of_construction()
+        self.assertEqual(
+            date_of_construction,
+            get_quarter_verbose(date_of_construction_of_building)
+        )
 
-'''
-class QuestionMethodTests(TestCase):
+    def test_get_date_of_construction_with_building_no_active(self):
+        building = create_building(
+            self.RC,
+            date_of_construction=datetime.datetime.now(),
+            is_active=False,
+        )
 
-    def test_was_published_recently_with_future_question(self):
-        """
-        was_published_recently() should return False for questions whose
-        pub_date is in the future.
-        """
-        time = timezone.now() + datetime.timedelta(days=30)
-        future_question = Question(pub_date=time)
-        self.assertEqual(future_question.was_published_recently(), False)
-'''
+        date_of_construction = self.RC.get_date_of_construction()
+        self.assertEqual(date_of_construction, NULL_DATE_OF_CONSTRUCTION)
+
+    @tag('slow')
+    def test_get_date_of_construction_with_mixed_buildings(self):
+        [
+            building1,
+            building2,
+            building3,
+            building_no_active1,
+            building_no_active2,
+        ] = buildings_factory(self.RC, 3, 2)
+
+        nearest_date = building1.date_of_construction
+        date_of_construction = self.RC.get_date_of_construction()
+        self.assertEqual(
+            date_of_construction,
+            get_quarter_verbose(nearest_date),
+        )
