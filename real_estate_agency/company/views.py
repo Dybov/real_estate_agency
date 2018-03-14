4d2015312ba06516e7402f77b341f690a623d47b
Andrey Dybov <dybov.andrew@gmail.com>
2018-03-15 01:49:08 +0500

    Change banks ordering.
    Make it OrderedDict.

--- real_estate_agency/company/views.py
+++ real_estate_agency/company/views.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 from django.shortcuts import render
 from django.template import RequestContext
 
@@ -6,18 +8,18 @@ from new_buildings.models import Builder, ResidentalComplex, NewApartment
 
 # Temporary dict for bank partners
 # 'Bank Name' : 'Image filewithout extension placed in static/img/partners/bank' 
-BANK_PARTNERS = {
-    'ПАО «Сбербанк России»' : 'sberbank',
-    'ВТБ 24 (ПАО)' : 'vtb24',
-    'ПАО «Запсибкомбанк»' : 'zpsib',
-    'АКБ «Абсолют Банк»' : 'absolut',
-    'ПАО «ТРАНСКАПИТАЛБАНК»' : 'transkapital',
-    'АО «КБ ДельтаКредит»' : 'deltacredit',
-    'Банк УРАЛСИБ' : 'norooturalsib',
-    '«АК БАРС» Банк' : 'akbars',
-    'ПАО Банк «ФК Открытие»' : 'otkretie',
-    'АО «Россельхозбанк»' : 'rosselhoz',
-}
+BANK_PARTNERS = OrderedDict([
+    ('ПАО «Сбербанк России»' , 'sberbank'),
+    ('ВТБ (ПАО)' , 'vtb24'),
+    ('ПАО «Запсибкомбанк»' , 'zpsib'),
+    ('АКБ «Абсолют Банк»' , 'absolut'),
+    ('ПАО Банк «ФК Открытие»' , 'otkretie'),
+    ('АО «Россельхозбанк»' , 'rosselhoz'),
+    ('ПАО «ТРАНСКАПИТАЛБАНК»' , 'transkapital'),
+    ('АО «КБ ДельтаКредит»' , 'deltacredit'),
+    ('Банк УРАЛСИБ' , 'norooturalsib'),
+    ('«АК БАРС» Банк' , 'akbars'),
+])
 
 def about(request):
     return render(request,
