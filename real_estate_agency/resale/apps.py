c0f1d463a58091c9a55b3acd0ff853b1387575f1
Andrey Dybov <dybov.andrew@gmail.com>
2018-06-12 20:23:41 +0500

    Make simple resale apartment list view with model.
    Use a little optimized templates from front-end developer. Needs to be more optimized.
    Move css part of resales to resale app.
    Modify base templates for adding extra styles and rendering links to resale views.

--- real_estate_agency/resale/apps.py
+++ real_estate_agency/resale/apps.py
@@ -4,4 +4,4 @@ from django.utils.translation import ugettext_lazy as _
 
 class ResaleConfig(AppConfig):
     name = 'resale'
-    verbose_name = _('вторички')
+    verbose_name = _('каталог квартир')
