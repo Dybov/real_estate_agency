b3ef899a8626115bab19cdcb32ce974bb2a52ed5
Andrey Dybov <dybov.andrew@gmail.com>
2018-06-16 09:11:57 +0500

    Set up simple url and view for resale detailed page.

--- real_estate_agency/resale/views.py
+++ real_estate_agency/resale/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
+from .models import ResaleApartment
 
 
 def index(request):
-    return render(request, 'resale/index.html')
+    context = {'apartments': ResaleApartment.objects.filter(is_active=True),}
+    return render(request, 'resale/resale_list.html', context)
+
+def detailed(request, pk):
+    context = {'apartment':get_object_or_404(ResaleApartment, pk=pk)}
+    return render(request, 'resale/resale_detailed.html', context)
