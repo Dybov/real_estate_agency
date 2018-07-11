61c2208a8cbe5e2f64dd89a4054a4d9547b0307e
Andrey Dybov <dybov.andrew@gmail.com>
2018-07-11 21:34:18 +0500

    Skip telegram tests until it will be unblocked in Russia.

--- real_estate_agency/applications/tests.py
+++ real_estate_agency/applications/tests.py
@@ -1,9 +1,13 @@
+from unittest import skip
+
 from django.test import TestCase, tag, override_settings
 from django.conf import settings
 
 from .telegram_tasks import TOKEN, sendTelegramMessage
 
 
+@skip('Because Telegram is blocking in Russia with using web filters also. \
+    So it is not reachable. Skip it until it will be unblocked')
 @tag('telegram')
 class TelegramBotTests(TestCase):
     @override_settings(CELERY_ALWAYS_EAGER=True)
