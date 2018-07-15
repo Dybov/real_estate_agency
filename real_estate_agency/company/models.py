ec1cae9f98bf93f8fc86e71730d91b1dcf465303
Andrey Dybov <dybov.andrew@gmail.com>
2018-07-15 18:25:27 +0500

    In company.models create BankAdmin, modify Award.
    Both inherit abstract BaseDraggapbleImage to have possitbility of change the order.
    Both uses different thumbnails from real_estate.models.image.thumbnail_factory

--- real_estate_agency/company/models.py
+++ real_estate_agency/company/models.py
@@ -1,30 +1,17 @@
 from django.db import models
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 
-from real_estate.models import BasePropertyImage
+from real_estate.models.image import (
+    BaseDraggapbleImage,
+    thumbnail_factory,
+    thumbnail_64_64,
+    thumbnail_260_260
+)
 
-from imagekit.models.fields import ImageSpecField
-from imagekit.processors import ResizeToFit, ResizeToFill
 
-
-class Award(BasePropertyImage):
-    position = models.PositiveSmallIntegerField(default=0,
-                                                verbose_name=_('позиция'))
-    image_thumbnail = ImageSpecField(
-        processors=[
-            ResizeToFit(
-                320,
-                430,
-                mat_color=(255, 255, 255, 1)
-            )
-        ],
-        source='image',
-        format='JPEG',
-        options={
-            'quality': 60,
-            'progressive': True,
-        }
-    )
+class Award(BaseDraggapbleImage):
+    image_thumbnail = thumbnail_factory(320, 430)
+    thumbnail_admin = thumbnail_factory(90, 120)
 
     def __str__(self):
         return _('грамота №%(pk)s') % {'pk': self.pk}
@@ -33,3 +20,18 @@ class Award(BasePropertyImage):
         ordering = ['position']
         verbose_name = _('грамота')
         verbose_name_plural = _('грамоты')
+
+
+class BankPartner(BaseDraggapbleImage):
+    title = models.CharField(
+        verbose_name=_('название банка'),
+        max_length=127,
+    )
+    thumbnail_34_34 = thumbnail_factory(34, 34)
+    thumbnail_64_64 = thumbnail_64_64
+    thumbnail_260_260 = thumbnail_260_260
+
+    class Meta:
+        ordering = ('position',)
+        verbose_name = _('объект банк-партнер')
+        verbose_name_plural = _('банки-партнеры')
