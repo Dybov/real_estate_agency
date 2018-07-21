a60ff55dfda7ab4a8c52e2257032ec22c340fc4c
Andrew <dybov.andrew@gmail.com>
2018-07-21 23:30:35 +0500

    Create ApartmentFilterMixin for using CBV new b-gs and resale.
    Extend both resale and new b-gs CBV list from real_estate.views.ApartmentFilterMixin.
    
    Remove real_estate.urls

--- real_estate_agency/resale/views.py
+++ real_estate_agency/resale/views.py
@@ -3,16 +3,61 @@ from django.shortcuts import get_object_or_404
 from django.views.generic import ListView
 from django.views.generic.edit import FormMixin
 
-from .models import ResaleApartment
+from real_estate.views import ApartmentFilterMixin
+
+from .models import ResaleApartment, TransactionMixin
 from .forms import ResaleSearchForm
 
 
-class ResaleListView(FormMixin, ListView):
+class ResaleListView(ApartmentFilterMixin, FormMixin, ListView):
     form_class = ResaleSearchForm
     model = ResaleApartment
     context_object_name = 'apartments'
     template_name = 'resale/resale_list.html'
-    queryset = model.objects.filter(is_active=True, )
+    queryset = model.objects.filter(
+        is_active=True, status=TransactionMixin.ACTIVE)
+
+    def get(self, request, *args, **kwargs):
+        data = getattr(request, request.method)
+
+        form_class = self.get_form_class()
+        self.form = form_class(data)
+        self.object_list = self.get_queryset()
+        self.apartment_list = self.object_list
+        empty_list_flag = False
+
+        if data and self.form.is_valid():
+            self.standartApartmentFilter()
+
+            if self.apartment_list:
+                self.object_list = self.apartment_list
+            else:
+                empty_list_flag = True
+
+        context = self.get_context_data(
+            form=self.form,
+            empty_list_flag=empty_list_flag,
+        )
+        return self.render_to_response(context)
+
+    def standartApartmentFilter(self):
+        # Standart filters for rooms, total_area and price
+        super(ResaleListView, self).standartApartmentFilter()
+
+        # Any text filter searches for:
+        # neighbourhood, residental_complex and street
+        self.aparmentByAnyTextIContains(
+            fieldname='any_text',
+            model_fields=[
+                'neighbourhood__name',
+                'residental_complex__name',
+                'street__name',
+            ],
+        )
+
+        # Searches for neighbourhood
+        self.filterApartment(fieldname='neighbourhood',
+                             filter_name='neighbourhood')
 
 
 def detailed(request, pk):
