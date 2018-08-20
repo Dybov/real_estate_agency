897725fb5aa2cac4ac0cdc9384dd002742e7a604
Andrew <dybov.andrew@gmail.com>
2018-08-21 00:55:25 +0500

    Fix bug with using RussianPhoneNumberFormMixin in ModelForms.
    It didn't save PhoneNumber into PhoneNumberField in DB.
    Currently it save PhoneNumber throught Form.instance if it is exists.
    Also fix PEP8.

--- real_estate_agency/applications/forms.py
+++ real_estate_agency/applications/forms.py
@@ -1,11 +1,23 @@
 from django import forms
 from django.utils.translation import ugettext as _
-from django.core.exceptions import ValidationError
+
 from phonenumber_field.formfields import PhoneNumberField
+from phonenumber_field.phonenumber import PhoneNumber
 
 
 class RussianPhoneNumberFormMixin(object):
     PHONE_NUMBER_FIELD = 'phone_number'
+
+    def set_right_phone(self, phone_number):
+        phone = PhoneNumber.from_string(phone_number)
+        self.cleaned_data[self.PHONE_NUMBER_FIELD] = phone
+
+        if hasattr(self, 'instance'):
+            setattr(self.instance, self.PHONE_NUMBER_FIELD, phone)
+
+        # is_valid will run again
+        self._errors.pop(self.PHONE_NUMBER_FIELD, None)
+
     def is_valid(self, *args, **kwargs):
         valid = super(RussianPhoneNumberFormMixin, self).is_valid()
 
@@ -13,12 +25,10 @@ class RussianPhoneNumberFormMixin(object):
         raw_data = self.data.get(self.PHONE_NUMBER_FIELD)
         if not self.cleaned_data.get(self.PHONE_NUMBER_FIELD):
             transformed8 = transform_russian8_phone_number(raw_data)
-            if transformed8!=raw_data:
-                self.cleaned_data[self.PHONE_NUMBER_FIELD] = transformed8
-                self._errors.pop(self.PHONE_NUMBER_FIELD, None)
+            if transformed8 != raw_data:
+                self.set_right_phone(transformed8)
                 valid = super(RussianPhoneNumberFormMixin, self).is_valid()
         return valid
-
 
 
 class CallbackForm(RussianPhoneNumberFormMixin, forms.Form):
@@ -32,11 +42,11 @@ class CallbackForm(RussianPhoneNumberFormMixin, forms.Form):
         label=_('Номер телефона'),
         widget=forms.TextInput(attrs={'placeholder': '+79995475707'}),
     )
-    extra_info = forms.CharField(widget = forms.HiddenInput(), required = False)
+    extra_info = forms.CharField(widget=forms.HiddenInput(), required=False)
 
 
 def transform_russian8_phone_number(phone_number):
     # It is commonplace to call by 8 (983)... against +7 (983)...
-    if phone_number and phone_number[0]=='8':
+    if phone_number and phone_number[0] == '8':
         phone_number = '+7' + phone_number[1:]
     return phone_number
