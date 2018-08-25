a8f01ca3d3e66226fa509b9892af075791d52d04
Andrew <dybov.andrew@gmail.com>
2018-08-26 02:12:57 +0500

    Extend User Model.
    Add fields, override get_full_name and get_short_name methods.

--- real_estate_agency/accounts/models.py
+++ real_estate_agency/accounts/models.py
@@ -12,33 +12,46 @@ from django.core.mail import send_mail
 from django.utils import six, timezone
 from django.utils.translation import ugettext_lazy as _
 
+from phonenumber_field.modelfields import PhoneNumberField
 
-class RealEstateUser(AbstractBaseUser, PermissionsMixin):
+from real_estate.models.helper import get_file_path
+
+
+def get_username_validator():
+    return UnicodeUsernameValidator() if six.PY3 else ASCIIUsernameValidator()
+
+
+class AbstactRealEstateUser(AbstractBaseUser, PermissionsMixin):
     """
     An abstract base class implementing a fully featured User model with
     admin-compliant permissions.
 
     Username and password are required. Other fields are optional.
     """
-    username_validator = UnicodeUsernameValidator() if six.PY3 else ASCIIUsernameValidator()
+    username_validator = get_username_validator()
 
     username = models.CharField(
         _('username'),
         max_length=150,
         unique=True,
-        help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+        help_text=_(
+            'Required. 150 characters or fewer.\
+ Letters, digits and @/./+/-/_ only.'
+        ),
         validators=[username_validator],
         error_messages={
             'unique': _("A user with that username already exists."),
         },
     )
-    first_name = models.CharField(_('first name'), max_length=30, blank=True)
-    last_name = models.CharField(_('last name'), max_length=30, blank=True)
-    email = models.EmailField(_('email address'), blank=True)
+    first_name = models.CharField(_('first name'), max_length=30)
+    last_name = models.CharField(_('last name'), max_length=30)
+    email = models.EmailField(_('email address'))
     is_staff = models.BooleanField(
         _('staff status'),
-        default=False,
-        help_text=_('Designates whether the user can log into this admin site.'),
+        default=True,
+        help_text=_(
+            'Designates whether the user can log into this admin site.'
+        ),
     )
     is_active = models.BooleanField(
         _('active'),
@@ -59,11 +72,10 @@ class RealEstateUser(AbstractBaseUser, PermissionsMixin):
     class Meta:
         verbose_name = _('user')
         verbose_name_plural = _('users')
-        db_table = 'auth_user'
-        # abstract = True
-        
+        abstract = True
+
     def clean(self):
-        super(RealEstateUser, self).clean()
+        super(AbstactRealEstateUser, self).clean()
         self.email = self.__class__.objects.normalize_email(self.email)
 
     def get_full_name(self):
@@ -82,3 +94,56 @@ class RealEstateUser(AbstractBaseUser, PermissionsMixin):
         Sends an email to this User.
         """
         send_mail(subject, message, from_email, [self.email], **kwargs)
+
+
+class RealEstateUser(AbstactRealEstateUser):
+    patronymic = models.CharField(_('отчество'), max_length=30, blank=True)
+    phone_number = PhoneNumberField(
+        _('номер телефона'),
+        blank=True,
+    )
+    photo = models.ImageField(
+        verbose_name=_('фото'),
+        upload_to=get_file_path,
+        help_text=_('Будет отображаться на сайте при определенных настройках'),
+        blank=True,
+    )
+    bio = models.TextField(_('биография'), max_length=255, blank=True)
+    show_at_company_page = models.BooleanField(
+        _('Отображать на сайте'),
+        default=True,
+        help_text=_('Показывать ли контакт на странице с контактами компании\
+ и на прочих страницах, где это уместно?')
+    )
+
+    def get_full_name(self):
+        """
+        Returns the first_name plus the last_name plus patronymic,
+        with a space in between.
+        """
+        full_name = '%s %s' % (self.last_name, self.first_name)
+        if self.patronymic:
+            full_name += ' %s' % self.patronymic
+        return full_name.strip()
+    get_full_name.short_description = _("Полное имя")
+
+    def get_short_name(self):
+        "Returns the short name for the user."
+        short_name = '%s %s.' % (
+            self.last_name,
+            self.first_name[0],
+        )
+        if self.patronymic:
+            short_name += ' %s.' % self.patronymic[0]
+        return short_name.strip().title()
+    get_short_name.short_description = _("Сокращенное имя")
+
+    def get_phone_number(self):
+        return self.phone_number
+
+    def get_photo(self):
+        return self.photo
+
+    def __str__(self):
+        return self.get_short_name()
+
--- real_estate_agency/accounts/tests.py
+++ real_estate_agency/accounts/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+from .models import RealEstateUser
+
+
+class RealEstateUserTest(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            'testuser', 'test@mail.test', 'testpass',
+        )
+
+    def test_settings(self):
+        self.assertIsInstance(self.user, RealEstateUser)
+
+    def test_user_fields(self):
+        self.assertEqual(self.user.email, 'test@mail.test')
+        self.assertEqual(self.user.username, 'testuser')
+        self.assertTrue(self.user.check_password('testpass'))
