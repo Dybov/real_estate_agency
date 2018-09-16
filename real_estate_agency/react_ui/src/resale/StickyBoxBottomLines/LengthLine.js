f1aa5064bb60a4c717a952ad121d5b95ee85224e
SERVER Andrey Dybov <dybov.andrew@gmail.com>
2018-09-16 13:43:21 -0400

    Change decimal places from 0 to 1 for length fields (ceiling height).

--- real_estate_agency/react_ui/src/resale/StickyBoxBottomLines/LengthLine.js
+++ real_estate_agency/react_ui/src/resale/StickyBoxBottomLines/LengthLine.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import SimpleLine from './SimpleLine'
-import { Length0digits } from '../../localization/formats'
+import { Length } from '../../localization/formats'
 
 
 export default class LengthLine extends SimpleLine {
 	constructor(props){
 		super(props)
-		this.WrappedComponent = <Length0digits value={props.r} />
+		this.WrappedComponent = <Length value={props.r} decPlaces={1} />
 	}
 }
